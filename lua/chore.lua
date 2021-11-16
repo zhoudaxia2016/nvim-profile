fd3cff4a401e2801ab3e555cc16cc3ce8fbb14f9
zhoudaxia <muzhouz@gmail.com>
2021-11-16 23:17:53 +0800

    feat: 复制到系统剪贴板

---
+++ lua/chore.lua
@@ -0,0 +1,11 @@
+local jobstart = require('util.jobstart')
+function CopyToSystem()
+  local temp = vim.fn.tempname()
+  print(temp)
+  local fd = io.open(temp, 'w')
+  io.output(fd)
+  io.write(vim.fn.getreg('"'))
+  io.close(fd)
+  jobstart('clip.exe < ' .. temp)
+end
+vim.api.nvim_set_keymap('n', '<m-o>', '<Cmd>call v:lua.CopyToSystem()<cr>', { silent = true })
