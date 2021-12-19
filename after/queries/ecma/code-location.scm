275c63b19a1b0e4510fb73118ff2137fcca2a814
zhoudaxia <muzhouz@gmail.com>
2021-12-19 14:06:26 +0800

    feat: code location添加函数调用和icons

--- after/queries/ecma/code-location.scm
+++ after/queries/ecma/code-location.scm
@@ -1,3 +1,13 @@
 ((pair
     key: (_) @proper-name
   ) @scope-root)
+
+((call_expression
+    function: (_) @call-expression-name
+    arguments:
+      (arguments ([(function) (arrow_function)]))
+  ) @scope-root)
+
+((variable_declarator
+  name: (identifier) @object-name
+  value: (object)) @scope-root)
