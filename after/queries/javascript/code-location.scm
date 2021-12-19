c43ea3683d500e055aa8092c3060108df18d8834
zhoudaxia <muzhouz@gmail.com>
2021-12-19 12:45:48 +0800

    feat: code location

---
+++ after/queries/javascript/code-location.scm
@@ -0,0 +1,39 @@
+; inherits: ecma
+
+; Class
+((class_declaration
+  name: (identifier) @class-name
+  body: (class_body)) @scope-root)
+
+; Function
+((function_declaration
+  name: (identifier) @function-name
+  body: (statement_block)) @scope-root)
+
+; Method
+((method_definition
+  name: (property_identifier) @method-name
+  body: (statement_block)) @scope-root)
+
+; Arrow Function
+((variable_declarator
+  name: (identifier) @function-name
+  value: (arrow_function)) @scope-root)
+
+; Function Expression
+((variable_declarator
+  name: (identifier) @function-name
+  value: (function)) @scope-root)
+
+; Tests
+((expression_statement
+  (call_expression
+    function: (identifier)
+    arguments: (arguments
+      (string) @method-name
+      (arrow_function)))) @scope-root)
+
+; Arrow function methods
+((field_definition
+  property: (property_identifier) @method-name
+  value: (arrow_function)) @scope-root)
---
+++ lua/self-plugin/code-location.lua
@@ -0,0 +1,42 @@
+-- 参考 https://github.com/SmiteshP/nvim-gps
+local ts_utils = require("nvim-treesitter.ts_utils")
+local ts_parsers = require("nvim-treesitter.parsers")
+local ts_queries = require("nvim-treesitter.query")
+
+function GetCodeLocation()
+  local filelang = ts_parsers.ft_to_lang(vim.bo.filetype)
+  local code_location_query = ts_queries.get_query(filelang, "code-location")
+
+  if not code_location_query then
+    return "error"
+  end
+
+  local current_node = ts_utils.get_node_at_cursor()
+
+  local node_text = {}
+  local node = current_node
+
+  while node do
+    local iter = code_location_query:iter_captures(node, 0)
+    local capture_ID, capture_node = iter()
+
+    if capture_node == node then
+      if code_location_query.captures[capture_ID] == "scope-root" then
+
+        while capture_node == node do
+          capture_ID, capture_node = iter()
+        end
+        local capture_name = code_location_query.captures[capture_ID]
+        table.insert(node_text, 1, table.concat(ts_utils.get_node_text(capture_node), ' '))
+
+      end
+    end
+
+    node = node:parent()
+  end
+
+  local context = table.concat(node_text, ' > ')
+  print(context)
+end
+
+require('util').map('n', '<leader>p', ':call v:lua.GetCodeLocation()<cr>', { silent = false })
