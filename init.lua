a220058955115e0375b1fbdb1b079701a1b8459a
zhou <maozhongzhou@moyi365.com>
2021-12-29 09:16:13 +0800

    feat: nord参数

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ opt.runtimepath:prepend('~/.nvim')
 opt.runtimepath:append('~/.nvim/after')
 vim.o.packpath = o.runtimepath
 vim.g.mapleader = ' '
+vim.g.nord_borders = true
 cmd[[
   filetype indent on
   filetype plugin on
