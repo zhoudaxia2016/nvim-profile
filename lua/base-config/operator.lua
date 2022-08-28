bb69eebe555c0529805a3a221b991d4708d99740
zhoudaxia <muzhouz@gmail.com>
2022-08-28 22:21:40 +0800

    feat(operator): add surround operator

--- lua/base-config/operator.lua
+++ lua/base-config/operator.lua
@@ -1,13 +1,23 @@
 OperatorFunc = nil
 local jobstart = require('util.jobstart')
-local prefix = '<m-q>'
+local utils = require "nvim-treesitter.ts_utils"
+local function prefix(key)
+  return "<m-q><m-" .. key .. ">"
+end
+local pairs = {
+  ['"'] = '"',
+  ['('] = ')',
+  ['['] = ']',
+  ['{'] = '}',
+  ['<'] = '>'
+}
 local function newOperator(key, fn)
-  vim.keymap.set('n', prefix .. '<m-' .. key .. '>', function()
+  vim.keymap.set({'n', 'v'}, key, function()
     OperatorFunc = fn
     -- TODO use lua function: vim.o.operatorfunc = fn
     vim.o.operatorfunc = 'v:lua.OperatorFunc'
     return 'g@'
-  end, { expr = true, buffer = 0, silent = ture, noremap = true })
+  end, { expr = true, silent = ture, noremap = true })
 end
 
 local function getRange()
@@ -16,7 +26,7 @@ local function getRange()
   return start, stop
 end
 
-newOperator('o', function(type)
+newOperator(prefix('o'), function(type)
   vim.cmd "normal `[v`]y"
   local temp = vim.fn.tempname()
   local fd = io.open(temp, 'w')
@@ -24,4 +34,32 @@ newOperator('o', function(type)
   io.write(vim.fn.getreg('"'))
   io.close(fd)
   jobstart('clip.exe < ' .. temp)
+end)
+
+newOperator('<m-u><m-w>', function()
+  local getLine = vim.fn.line
+  local getCol = vim.fn.col
+  local parser = vim.treesitter.get_parser(0)
+  local tstree = parser:parse()
+  local startLine = getLine("'[") - 1
+  local startCol = getCol("'[") - 1
+  local stopLine = getLine("']") - 1
+  local stopCol = getCol("']") - 1
+  local node = tstree[1]:root():descendant_for_range(startLine, startCol, stopLine, stopCol)
+  local parent = node:parent()
+  local f = parent:child(0)
+  local l = parent:child(parent:child_count() - 1)
+  local fc = string.char(vim.fn.getchar())
+  local lc = pairs[fc]
+  if (lc == nil) then
+    lc = fc
+  end
+  local frange = utils.node_to_lsp_range(f)
+  local lrange = utils.node_to_lsp_range(l)
+  local edits = {
+    { newText = fc, range = frange },
+    { newText = lc, range = lrange }
+  }
+  print(vim.inspect(edits))
+  vim.lsp.util.apply_text_edits(edits, 0, 'utf-8')
 end)
