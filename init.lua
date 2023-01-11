b52e4eb1174422777902b2542416d93dd89616cb
zhoudaxia <muzhouz@gmail.com>
2023-01-11 22:55:00 +0800

    chore: 彻底删除旧配置

--- init.lua
+++ init.lua
@@ -1,21 +1,15 @@
 local opt = vim.opt
 local cmd = vim.cmd
 local o = vim.o
-vim.g.nord_italic = false
 
 vim.g.workspace = require('lspconfig.util').root_pattern('package.json', '.git')(vim.fn.getcwd()) or vim.fn.getcwd()
 
-opt.runtimepath:prepend('~/.nvim')
-opt.runtimepath:append('~/.nvim/after')
 vim.o.packpath = o.runtimepath
 vim.g.mapleader = ' '
+
 vim.g.nord_borders = true
-cmd[[
-  filetype indent on
-  filetype plugin on
-  syntax on
-  colors nord
-]]
+vim.g.nord_italic = false
+require('nord').set()
 
 require 'base-config'
 require 'plugin-config'
