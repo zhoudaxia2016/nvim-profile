e0e95be19549ff4d26b23b8ac52ede7da6619000
zhoudaxia <muzhouz@gmail.com>
2023-01-18 20:22:44 +0800

    fix(statuscolumn): gitsign优化当前buffer无需显示时返回空字符串 && statuscolumn长度优化

--- lua/self-plugin/gitsign.lua
+++ lua/self-plugin/gitsign.lua
@@ -1,4 +1,5 @@
 local gitsign_data = {}
+local memoize_by_buf_tick = require('nvim-treesitter.ts_utils').memoize_by_buf_tick
 local gitsign_config = {
   a = { hl = 'diffAdded', icon = '│' },
   db = { hl = 'diffRemoved', icon = '‾'},
@@ -7,9 +8,9 @@ local gitsign_config = {
   cd = { hl = 'diffChanged', icon = '~' },
 }
 
-local function isInsideGitWorkTree()
+local isInsideGitWorkTree = memoize_by_buf_tick(function()
   return vim.trim(vim.fn.system('git rev-parse --is-inside-work-tree')) == 'true'
-end
+end)
 
 local function getDiff()
   local originFile = vim.fn.system('git show --no-color HEAD:./' .. vim.fn.expand('%'))
@@ -24,6 +25,9 @@ local m = {}
 m.sign = function()
   local lnum = vim.v.lnum
   local buf = vim.api.nvim_get_current_buf()
+  if (isInsideGitWorkTree(buf) == false) then
+    return ''
+  end
   if (gitsign_data[buf]) then
     local config = gitsign_config[gitsign_data[buf][lnum]]
     return gitsign_data[buf][lnum] and string.format('%%#%s#%s', config.hl, config.icon) or ' '
@@ -34,11 +38,11 @@ end
 m.setup = function()
   vim.api.nvim_create_autocmd({'BufEnter', 'TextChanged', 'TextChangedI'}, {
     callback = function()
-      if (isInsideGitWorkTree() == false or vim.o.filetype == 'netrw') then
+      local buf = vim.api.nvim_get_current_buf()
+      if (isInsideGitWorkTree(buf) == false or vim.o.filetype == 'netrw') then
         return
       end
       local diff = getDiff()
-      local buf = vim.api.nvim_get_current_buf()
       gitsign_data[buf] = {}
       for _, chunk in ipairs(diff) do
         local status = 'c'
