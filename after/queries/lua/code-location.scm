f18e0011be3f0325cc516e5e66817b47220f1842
zhoudaxia <muzhouz@gmail.com>
2023-01-18 20:24:16 +0800

    fix(code-location): 优化query

--- after/queries/lua/code-location.scm
+++ after/queries/lua/code-location.scm
@@ -3,9 +3,9 @@
     (variable_list (identifier) @function-name)
     (expression_list (function_definition)))) @scope-root
 
-(function_declaration [(identifier) (dot_index_expression)] @function-name) @scope-root
+(function_declaration [(identifier) (dot_index_expression) (method_index_expression)] @function-name) @scope-root
 
 (function_call
-  [(identifier) (dot_index_expression)] @call-expression-name) @scope-root
+  [(identifier) (dot_index_expression) (method_index_expression)] @call-expression-name) @scope-root
 
 (field [(identifier) (string)] @proper-name) @scope-root
