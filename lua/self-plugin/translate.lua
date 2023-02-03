fd7609c68dbaee5b66adb9cbb5ac1ca276fe50f2
zhoudaxia <muzhouz@gmail.com>
2023-02-03 23:27:49 +0800

    feat(translate): 完善翻译插件，使用baidu api

--- lua/self-plugin/translate.lua
+++ lua/self-plugin/translate.lua
@@ -1,26 +1,59 @@
+local config = vim.g.baiduApi
+if (config == nil or config.appId == nil or config.appKey == nil) then
+  vim.notify('[Translate] please config your appId and appKey...')
+  return
+end
+local appId = config.appId
+local appKey = config.appKey
+
 local cmd, api, fn = vim.cmd, vim.api, vim.fn
+local curl = require('plenary.curl')
+local random = tostring(math.random(bit.rshift(1, 5)))
+local uri = 'https://fanyi-api.baidu.com/api/trans/vip/translate'
+
 cmd('hi Translate guifg=#bbded6 guibg=transparent')
-function Translate()
-  cmd 'normal! viwy'
-  local result = {}
-  local id
-  local function callback(_, r)
-    table.insert(result, r)
-  end
-  local function close()
-    table.foreach(result, function(k, v) result[k] = string.gsub(v, "^%s*(.-)%s*$", "%1") end)
-    local buf = api.nvim_create_buf(false, true)
-    api.nvim_buf_set_lines(buf, 0, -1, true, result)
-    id = api.nvim_open_win(buf, false, { style = 'minimal', relative = 'cursor', row = 1, col = 0, width = 10, height = 3, border = 'single' })
-    api.nvim_win_set_option(id, 'winhl', 'Normal:Translate')
-    cmd("au CursorMoved,CmdLineEnter * ++once call v:lua.Translate_handleCursorMove()")
-  end
-  function Translate_handleCursorMove()
-    if id and api.nvim_win_is_valid(id) then
-      api.nvim_win_close(id, true)
-    end
-  end
-  fn['job#start']('trans -no-ansi :zh --brief ' .. fn.getreg('0'), { out_cb = callback, close_cb = close })
+
+local getField = function(word)
+  local curtime = tostring(os.time())
+  local salt = random .. curtime
+  local hash = appId .. word .. salt .. appKey
+  local sign = vim.fn.sha256(hash)
+  return {
+    q = word,
+    from = 'en',
+    to = 'zh',
+    signType = 'v3',
+    appid = appId,
+    salt = salt,
+    sign = sign,
+  }
 end
 
-vim.api.nvim_set_keymap('n', '<leader>t', ':call v:lua.Translate()<cr>', {})
+vim.keymap.set('n', '<leader>t', function()
+  cmd 'normal! viwy'
+  local word = fn.getreg('0')
+  local field = getField(word)
+  curl.post(uri, {
+    body = field,
+    callback = function(output)
+      local result = vim.json.decode(output.body)
+      if (result == nil or result.trans_result == nil) then
+        return
+      end
+      vim.schedule(function()
+        local buf = api.nvim_create_buf(false, true)
+        api.nvim_buf_set_lines(buf, 0, -1, true, {result.trans_result[1].dst})
+        local id = api.nvim_open_win(buf, false, { style = 'minimal', relative = 'cursor', row = 1, col = 0, width = 10, height = 3, border = 'single' })
+        api.nvim_win_set_option(id, 'winhl', 'Normal:Translate')
+        vim.api.nvim_create_autocmd({'CursorMoved', 'CmdLineEnter'}, {
+          callback = function()
+            if id and api.nvim_win_is_valid(id) then
+              api.nvim_win_close(id, true)
+            end
+          end,
+          once = true,
+        })
+      end)
+    end
+  })
+end)
