08405e64cfda1a8c104e4a85786124597b0932e4
zhoudaxia <muzhouz@gmail.com>
2023-02-03 18:42:38 +0800

    feat: turn to left tab after closing tab

--- lua/base-config/autocmd.lua
+++ lua/base-config/autocmd.lua
@@ -4,3 +4,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank { higroup='DiffText', timeout=700 }
   end
 })
+
+local lastClosedTab
+vim.api.nvim_create_autocmd('TabLeave', {
+  pattern = '*',
+  callback = function()
+    lastClosedTab = vim.fn.tabpagenr()
+  end
+})
+vim.api.nvim_create_autocmd('TabClosed', {
+  pattern = '*',
+  callback = function()
+    local curtab = vim.fn.tabpagenr()
+    if (curtab == 1) then
+      return
+    end
+    print(curtab, lastClosedTab)
+    if (curtab == lastClosedTab) then
+      local tabonleft = curtab - 1
+      vim.cmd(tabonleft .. 'tabnext')
+    end
+  end
+})
