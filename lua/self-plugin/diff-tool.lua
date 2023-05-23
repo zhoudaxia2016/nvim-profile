525eecb9e1a74aaf6cd4f9cb3fe3fa987b12ed65
zhoudaxia <muzhouz@gmail.com>
2023-05-23 23:35:32 +0800

    feat(git): diff tool

--- lua/self-plugin/diff-tool.lua
+++ lua/self-plugin/diff-tool.lua
@@ -9,3 +9,18 @@ vim.api.nvim_create_user_command('MergeTool', function()
         diffthis
   ]]
 end, {})
+
+vim.api.nvim_create_user_command('Diff', function(params)
+  local args = params.args
+  local tmpPatchFile = vim.fn.tempname()
+  local cmd = 'git diff --no-color '
+  if args == '' then
+    cmd = cmd .. '-R '
+  else
+    cmd = cmd .. '..' .. args .. ' -- ' .. vim.fn.expand('%')
+  end
+  cmd = cmd .. ' > ' .. tmpPatchFile
+  vim.fn.system(cmd)
+  vim.cmd('silent vert diffpatch ' .. tmpPatchFile)
+  vim.o.bufhidden = 'delete'
+end, {nargs = '?'})
