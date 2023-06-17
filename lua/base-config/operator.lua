60265d1b1ec6f5ba00fa653111bcae07bc309a5f
zhoudaxia <muzhouz@gmail.com>
2023-06-17 16:55:22 +0800

    feat: operator调整

--- lua/base-config/operator.lua
+++ lua/base-config/operator.lua
@@ -1,9 +1,5 @@
 OperatorFunc = nil
-local jobstart = require('util.jobstart')
 local utils = require "nvim-treesitter.ts_utils"
-local function prefix(key)
-  return "<m-q><m-" .. key .. ">"
-end
 local pairs = {
   ['"'] = '"',
   ['('] = ')',
@@ -11,8 +7,9 @@ local pairs = {
   ['{'] = '}',
   ['<'] = '>'
 }
+local prefix = '<leader>o'
 local function newOperator(key, fn)
-  vim.keymap.set({'n', 'v'}, key, function()
+  vim.keymap.set({'n', 'v'}, prefix .. key, function()
     OperatorFunc = fn
     -- TODO use lua function: vim.o.operatorfunc = fn
     vim.o.operatorfunc = 'v:lua.OperatorFunc'
@@ -20,15 +17,10 @@ local function newOperator(key, fn)
   end, { expr = true, silent = true, noremap = true })
 end
 
-local function getRange()
-  local start = vim.fn.line("'['")
-  local stop = vim.fn.line("']'")
-  return start, stop
-end
+local getLine = vim.fn.line
+local getCol = vim.fn.col
 
-newOperator('<m-u><m-w>', function()
-  local getLine = vim.fn.line
-  local getCol = vim.fn.col
+newOperator('s', function()
   local parser = vim.treesitter.get_parser(0)
   local tstree = parser:parse()
   local startLine = getLine("'[") - 1
@@ -36,20 +28,21 @@ newOperator('<m-u><m-w>', function()
   local stopLine = getLine("']") - 1
   local stopCol = getCol("']") - 1
   local node = tstree[1]:root():descendant_for_range(startLine, startCol, stopLine, stopCol)
-  local parent = node:parent()
-  local f = parent:child(0)
-  local l = parent:child(parent:child_count() - 1)
   local fc = string.char(vim.fn.getchar())
   local lc = pairs[fc]
   if (lc == nil) then
     lc = fc
   end
-  local frange = utils.node_to_lsp_range(f)
-  local lrange = utils.node_to_lsp_range(l)
+  local range = utils.node_to_lsp_range(node)
   local edits = {
-    { newText = fc, range = frange },
-    { newText = lc, range = lrange }
+    { newText = fc .. vim.treesitter.get_node_text(node, 0) .. lc, range = range },
   }
-  print(vim.inspect(edits))
   vim.lsp.util.apply_text_edits(edits, 0, 'utf-8')
+end)
+
+newOperator('b', function()
+  local startLine = getLine("'[") - 1
+  local stopLine = getLine("']") - 1
+  local cmd = string.format('git log -L %d,%d:%s', startLine, stopLine, vim.fn.expand('%'))
+  vim.print(vim.fn.system(cmd))
 end)
