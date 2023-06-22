6658dd0b9d996f8288944b596f939081c349ae38
zhoudaxia <muzhouz@gmail.com>
2023-06-22 17:15:32 +0800

    feat: auto require支持目录

--- lua/util/loadModule.lua
+++ lua/util/loadModule.lua
@@ -1,9 +1,14 @@
 -- Load all module from a directory
 return function(dir)
   local scan = require'plenary.scandir'
-  local files = scan.scan_dir(vim.env.HOME .. '/.config/nvim/lua/' .. dir, { depth = 1 })
+  local files = scan.scan_dir(vim.env.HOME .. '/.config/nvim/lua/' .. dir, { depth = 1, add_dirs = 1 })
   for i = 1, #files do
-    local f = files[i]:match("^.+/(.+)%..+$")
+    local f
+    if vim.fn.isdirectory(files[i]) == 1 then
+      f = files[i]:match("^.+/(.+)$")
+    else
+      f = files[i]:match("^.+/(.+)%..+$")
+    end
     if f ~= 'init' then
       require(dir .. '.' .. f)
     end
