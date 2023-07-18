516366b6dd904ce183e11d39e242e7ddceaa58b7
zhoudaxia <muzhouz@gmail.com>
2023-07-18 23:21:54 +0800

    feat(fzf): 优化大文件检测

--- lua/self-plugin/fzf/previewer.lua
+++ lua/self-plugin/fzf/previewer.lua
@@ -9,12 +9,11 @@ M.file = function(params)
   local hlRow = params.hlRow
   local currentFn = vim.fn.expand('%:p')
   local buf = params.buf
-  local needCheckSize = true
-  if fn and (fn:match('^Man://') or vim.fn.isdirectory(fn)) == 1 then
-    needCheckSize = false
-  end
-  if needCheckSize then
-    local file_contents = fn and vim.fn.readfile(fn) or vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  -- TODO: 更准确的大文件检测
+  local ok, file_contents = pcall(function()
+    return fn and vim.fn.readfile(fn) or vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  end)
+  if ok then
     local file_length = #vim.fn.join(file_contents, ' ')
     if file_length > 300000 then
       local messageBuf = vim.api.nvim_create_buf(false, true)
