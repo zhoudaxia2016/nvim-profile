f84221169979ed0eb567db4c468ccf501f068055
zhoudaxia <muzhouz@gmail.com>
2023-11-29 18:39:24 +0800

    feat(jest): 支持批量

--- lua/self-plugin/jest-runner.lua
+++ lua/self-plugin/jest-runner.lua
@@ -1,12 +1,20 @@
 local ts_utils = require('nvim-treesitter.ts_utils')
+local get_node_text = vim.treesitter.get_node_text
 
 local function getTestName(node)
   if node == nil then return nil end
   if node:type() == 'call_expression' then
     local children = ts_utils.get_named_children(node)
-    local text = ts_utils.get_node_text(children[1])[1]
+    local text = get_node_text(children[1], 0)
+    local name = ''
     if text == 'describe' or text == 'it' then
-      return ts_utils.get_node_text(ts_utils.get_named_children(children[2])[1])[1]
+      name = get_node_text(ts_utils.get_named_children(ts_utils.get_named_children(children[2])[1])[1], 0)
+      if text == 'it' then
+        name = getTestName(node:parent()) .. " " .. name
+      end
+      return name
+    else
+      return getTestName(node:parent())
     end
   end
   return getTestName(node:parent())
@@ -32,14 +40,14 @@ local function runJestTest(debug)
   loadedConfig = true
 
   local node = ts_utils.get_node_at_cursor()
-  local name = getTestName(node)
+  local name = getTestName(node) or ''
   local fn = vim.fn.expand('%:p')
   local debugArgs = ''
   if debug == 1 then
     debugArgs = '--inspect-brk'
   end
   vim.cmd[[tabnew]]
-  vim.fn.termopen(string.format('node %s %s %s -c %s -t %s', debugArgs, bin, fn, configFile, name), { cwd = workspace })
+  vim.fn.termopen(string.format('node %s %s %s -c %s -t "%s"', debugArgs, bin, fn, configFile, name), { cwd = workspace })
 end
 
 require('util').map('n', '<m-t><m-t>', function()
