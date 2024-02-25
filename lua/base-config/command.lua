b7cd2f9b649e88f1f2b8744dff4151e377318d6f
zhoudaxia <muzhouz@gmail.com>
2024-02-25 22:53:36 +0800

    feat: autosave

--- lua/base-config/command.lua
+++ lua/base-config/command.lua
@@ -7,3 +7,18 @@ end, { desc = 'Edit treesitter query', nargs = '?', complete = function(args)
     end, installedParsers)
   end
 })
+local autoSaveAutoCmdId
+vim.api.nvim_create_user_command('ToggleAutoSave', function()
+  if autoSaveAutoCmdId then
+    vim.api.nvim_del_autocmd(autoSaveAutoCmdId)
+    autoSaveAutoCmdId = nil
+    vim.notify('AutoSave Mode is closed!')
+  else
+    autoSaveAutoCmdId = vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+      pattern = { "*" },
+      command = "silent! w",
+      nested = true,
+    })
+    vim.notify('AutoSave Mode is opened!')
+  end
+end, {})
