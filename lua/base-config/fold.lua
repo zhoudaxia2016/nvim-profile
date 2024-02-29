14719b1520d256f00c339359b03c459995126c20
zhoudaxia <muzhouz@gmail.com>
2024-02-29 18:57:42 +0800

    chore

--- lua/base-config/fold.lua
+++ lua/base-config/fold.lua
@@ -8,7 +8,7 @@ opt.foldopen:append('hor')
 vim.o.foldtext = ''
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldlevel = 2
+vim.o.foldlevel = 1
 vim.o.foldminlines = 16
 
 vim.api.nvim_create_autocmd('FileType', {
