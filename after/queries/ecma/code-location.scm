845b4ce5ca81def24cf6280318dc0f4cb373396c
zhoudaxia <muzhouz@gmail.com>
2025-01-23 18:18:47 +0800

    fix(code-location): new Promise显示错误

--- after/queries/ecma/code-location.scm
+++ after/queries/ecma/code-location.scm
@@ -14,6 +14,14 @@
       (arguments ([(function_expression) (arrow_function)]))
   ) @scope-root)
 
+((call_expression
+    function: (member_expression
+      object:(new_expression
+        constructor: (identifier) @call-expression-name))
+    arguments:
+      (arguments ([(function_expression) (arrow_function)]))
+  ) @scope-root)
+
 ((variable_declarator
   name: (identifier) @object-name
   value: (object)) @scope-root)
