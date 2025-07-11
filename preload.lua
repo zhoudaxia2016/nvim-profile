bb0a43009491b3f5e7026f82d3efe9eb587c1e28
zhoudaxia <muzhouz@gmail.com>
2025-07-11 23:50:14 +0800

    feat: 引入nvim包管理，删除gitmodule包管理

---
+++ preload.lua
@@ -0,0 +1,13 @@
+vim.pack.add({
+  -- base
+  { src = 'https://github.com/lukas-reineke/indent-blankline.nvim.git' },
+  { src = 'https://github.com/shaunsingh/nord.nvim.git' },
+  { src = 'https://github.com/windwp/nvim-autopairs.git' },
+  -- official
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter.git', version = 'e5c8398e449281' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter-refactor.git' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git' },
+  { src = 'https://github.com/neovim/nvim-lspconfig.git', version = 'ba1057cba0' },
+  -- dev
+  { src = 'https://github.com/nvim-lua/plenary.nvim.git' },
+})
