601e32205ee9bad64abedb83f00dd298f8feb954
zhoudaxia <muzhouz@gmail.com>
2025-12-20 16:39:32 +0800

    feat: fold配置

--- lua/base-config/treesitter.lua
+++ lua/base-config/treesitter.lua
@@ -1,4 +1,14 @@
-vim.o.foldlevel = 3
+local opt = vim.opt
+
+opt.foldopen:append('jump')
+opt.foldopen:append('search')
+opt.foldopen:append('hor')
+
+vim.o.foldtext = ''
+vim.opt.foldmethod = "expr"
+vim.o.foldlevel = 1
+vim.o.foldminlines = 16
+
 vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
