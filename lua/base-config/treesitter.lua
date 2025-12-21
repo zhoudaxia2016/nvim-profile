0ff0776bf5af96f92dc64cfebaaa2ca41df84415
maozhongzhou <maozhongzhou@kingsoft.com>
2025-12-21 16:44:26 +0800

    feat: 一些不支持treesitter indent的文件类型不设置treesitter的indentexpr

--- lua/base-config/treesitter.lua
+++ lua/base-config/treesitter.lua
@@ -13,6 +13,10 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo[0][0].foldmethod = 'expr'
+    local not_support_indent_fts = {'gitconfig', 'kotlin', 'html', 'vim', 'json5'}
+    if vim.tbl_contains(not_support_indent_fts, vim.o.filetype) then
+      return
+    end
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end
 })
