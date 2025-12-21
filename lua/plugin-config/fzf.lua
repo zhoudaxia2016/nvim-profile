fd9846a0ce59d298cd196762b51057c6fd4bd7a1
zhoudaxia <muzhouz@gmail.com>
2025-12-21 14:34:31 +0800

    fix: qf buffer不要映射fzf快捷键

--- lua/plugin-config/fzf.lua
+++ lua/plugin-config/fzf.lua
@@ -1,31 +1,74 @@
 local fzfBuiltins = require('self-plugin.fzf.builtin')
 local getRoot = require('util').getRoot
 
-vim.keymap.set('n', '<cr>E', function()
-  fzfBuiltins.findFile(vim.fn.getcwd())
-end, {desc = 'Search files in current directory'})
-vim.keymap.set('n', '<cr>e', function()
-  fzfBuiltins.findFile(getRoot())
-end, {desc = 'Search files in current projects'})
+---@param lhs string
+---@param rhs string|function
+---@param opts? vim.keymap.set.Opts
+local function nmap(lhs, rhs, opts)
+  vim.keymap.set('n', lhs, rhs, vim.tbl_extend('force', {buffer = true}, opts))
+end
 
-vim.keymap.set('n', '<cr>F', function()
-  fzfBuiltins.rgSearch(vim.fn.getcwd())
-end, {desc = 'Search with rg in current directory'})
-vim.keymap.set('n', '<cr>f', function()
-  fzfBuiltins.rgSearch(getRoot())
-end, {desc = 'Search with rg in current projects'})
-vim.keymap.set('n', '<cr><m-f>', function()
-  vim.ui.input({ prompt = 'Enter ripgrep options: '}, function(input)
-    if input == nil then
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    local ignore_fts = {'qf'}
+    if vim.tbl_contains(ignore_fts, vim.o.filetype) then
       return
     end
-    fzfBuiltins.rgSearch(getRoot(), input)
-  end)
-end, {desc = 'Search with rg and options'})
+    nmap('<cr>E', function()
+      fzfBuiltins.findFile(vim.fn.getcwd())
+    end, {desc = 'Search files in current directory'})
+    nmap('<cr>e', function()
+      fzfBuiltins.findFile(getRoot())
+    end, {desc = 'Search files in current projects'})
 
-vim.keymap.set('n', '<c-f>l', function()
-  fzfBuiltins.searchLines()
-end, {desc = 'Search lines in current buffer'})
+    nmap('<cr>F', function()
+      fzfBuiltins.rgSearch(vim.fn.getcwd())
+    end, {desc = 'Search with rg in current directory'})
+    nmap('<cr>f', function()
+      fzfBuiltins.rgSearch(getRoot())
+    end, {desc = 'Search with rg in current projects'})
+    nmap('<cr><m-f>', function()
+      vim.ui.input({ prompt = 'Enter ripgrep options: '}, function(input)
+        if input == nil then
+          return
+        end
+        fzfBuiltins.rgSearch(getRoot(), input)
+      end)
+    end, {desc = 'Search with rg and options'})
+
+    nmap('<c-f>l', function()
+      fzfBuiltins.searchLines()
+    end, {desc = 'Search lines in current buffer'})
+
+    nmap('<c-f>r', function()
+      fzfBuiltins.oldFiles()
+    end, {desc = 'Show old files'})
+
+    nmap('<cr>b', function()
+      fzfBuiltins.buffers()
+    end, {desc = 'Show all buffers and jump to'})
+
+    nmap('<cr>c', function()
+      fzfBuiltins.clearBuffer()
+    end, {desc = 'Show all buffers and clear'})
+
+    nmap('<cr>j', function()
+      fzfBuiltins.jumps()
+    end, {desc = 'Show jumps'})
+
+    nmap('<cr>m', function()
+      fzfBuiltins.changes()
+    end, {desc = 'Show changes'})
+
+    nmap('<cr>a', function()
+      fzfBuiltins.nvimApis()
+    end, {desc = 'Show nvim apis'})
+
+    nmap('<cr>z', function()
+      fzfBuiltins.z()
+    end, {desc = 'Show directorys recently visited'})
+  end
+})
 
 vim.api.nvim_create_autocmd('VimEnter', {
   pattern = '*',
@@ -36,33 +79,6 @@ vim.api.nvim_create_autocmd('VimEnter', {
     end
   end
 })
-vim.keymap.set('n', '<c-f>r', function()
-  fzfBuiltins.oldFiles()
-end, {desc = 'Show old files'})
-
-vim.keymap.set('n', '<cr>b', function()
-  fzfBuiltins.buffers()
-end, {desc = 'Show all buffers and jump to'})
-
-vim.keymap.set('n', '<cr>c', function()
-  fzfBuiltins.clearBuffer()
-end, {desc = 'Show all buffers and clear'})
-
-vim.keymap.set('n', '<cr>j', function()
-  fzfBuiltins.jumps()
-end, {desc = 'Show jumps'})
-
-vim.keymap.set('n', '<cr>m', function()
-  fzfBuiltins.changes()
-end, {desc = 'Show changes'})
-
-vim.keymap.set('n', '<cr>a', function()
-  fzfBuiltins.nvimApis()
-end, {desc = 'Show nvim apis'})
-
-vim.keymap.set('n', '<cr>z', function()
-  fzfBuiltins.z()
-end, {desc = 'Show directorys recently visited'})
 
 vim.api.nvim_create_user_command('FzfKeymaps', function()
   fzfBuiltins.keymaps()
