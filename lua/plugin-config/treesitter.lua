9d9ae598cf99786b768a377cef62814e58d9984d
maozhongzhou <maozhongzhou@kingsoft.com>
2025-12-25 00:14:00 +0800

    feat: start treesitter highlighter

--- lua/plugin-config/treesitter.lua
+++ lua/plugin-config/treesitter.lua
@@ -1,1 +1,11 @@
-require'nvim-treesitter'.install {"javascript", "typescript", "tsx", "lua", "json", "jsonc", "query", "comment", "scheme", "markdown", "markdown_inline", "toml", "rust", "go", "c", "cpp", "git_config", "kotlin", "java"}
+local langs = {"javascript", "typescript", "tsx", "lua", "json", "jsonc", "query", "comment", "scheme", "markdown", "markdown_inline", "toml", "rust", "go", "c", "cpp", "git_config", "kotlin", "java"}
+
+require'nvim-treesitter'.install(langs)
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    local lang = vim.treesitter.language.get_lang(vim.o.filetype)
+    if vim.tbl_contains(langs, lang) then
+      vim.treesitter.start(args.buf, lang)
+    end
+  end
+})
