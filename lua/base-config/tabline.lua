b261caddbc69d290bf6cfd9d9c682a1de05b5940
zhoudaxia <muzhouz@gmail.com>
2026-01-02 18:11:48 +0800

    feat: 完善tabline
    - 右边显示当前tab索引和总tab数
    - 让当前tab必出现在可视区域

--- lua/base-config/tabline.lua
+++ lua/base-config/tabline.lua
@@ -1,31 +1,21 @@
+local loadColorSet = require('nord.util').loadColorSet
+local palettes = require('nord.named_colors')
+
+local themes = {
+  ['TabLineInfoFill'] = { fg = palettes.purple },
+  ['TabLineInfo'] = { fg = palettes.purple, bg = palettes.dark_gray },
+}
+loadColorSet(themes)
+
 local fn = vim.fn
 local api = vim.api
-function MyTabLine()
-  local s = ''
-  for i, tabpage in pairs(api.nvim_list_tabpages()) do
-    -- select the highlighting
-    if i == fn.tabpagenr() then
-      s = s .. '%#TabLineSel#'
-    else
-      s = s .. '%#TabLine#'
-    end
 
-    -- the label is made by MyTabLabel()
-    s = s .. string.format(' %s ', MyTabLabel(tabpage))
-  end
+local start = 1
+local stop = 1
+local start_len = -1
+local stop_len = -1
 
-  -- after the last tab fill with TabLineFill and reset tab page nr
-  s = s .. '%#TabLineFill#%T'
-
-  -- right-align the label to close the current tab page
-  if fn.tabpagenr('$') > 1 then
-    s = s .. '%=%#TabLine#%'
-  end
-
-  return s
-end
-
-function MyTabLabel(n)
+local function tabLabel(n)
   local winlist = api.nvim_tabpage_list_wins(n)
   local activeBuf
   if #winlist == 1 then
@@ -43,4 +33,83 @@ function MyTabLabel(n)
   local modifyMark = isModify and '+ ' or ''
   return modifyMark .. fn.fnamemodify(api.nvim_buf_get_name(activeBuf), ':t')
 end
+
+function MyTabLine()
+  local tabpages = {}
+  local select_index = fn.tabpagenr()
+  local count = fn.tabpagenr('$')
+  local info = string.format(' [%s/%s] ', select_index, count)
+  local capacity = vim.o.columns - #info
+  for _, tabpage in pairs(api.nvim_list_tabpages()) do
+    local text = string.format(' %s ', tabLabel(tabpage))
+    table.insert(tabpages, { text = text })
+  end
+
+  local total_char = 0
+
+  -- 当前选中tab不在可视区域，重新计算
+  if select_index < start or select_index > stop then
+    start = select_index
+    stop = select_index
+    total_char = #tabpages[select_index].text
+  else
+    for i = start, stop do
+      total_char = total_char + #tabpages[i].text
+    end
+  end
+
+  -- 还有空间，往前显示
+  while total_char < capacity and start > 1 do
+    start = start - 1
+    total_char = total_char + #tabpages[start].text
+  end
+  -- 还有空间，往后显示
+  while total_char < capacity and stop < count do
+    stop = stop + 1
+    total_char = total_char + #tabpages[stop].text
+  end
+
+  -- 超出屏幕，裁剪
+  if total_char > capacity then
+    if stop == select_index then
+      start_len = #tabpages[start].text - (total_char - capacity)
+      stop_len = -1
+    else
+      stop_len = #tabpages[stop].text - (total_char - capacity)
+      start_len = -1
+    end
+  end
+
+  local s = ''
+  for i = start, stop do
+    if i == select_index then
+      s = s .. '%#TabLineSel#'
+    else
+      s = s .. '%#TabLine#'
+    end
+    local tabpage = tabpages[i]
+    local text = tabpage.text
+    if i == start and start_len ~= -1 then
+      text = '<' .. string.sub(text, #text - start_len, #text)
+    end
+    if i == stop and stop_len ~= -1 then
+      text = string.sub(text, 1, stop_len - 1) .. '>'
+    end
+    s = s .. text
+  end
+
+  -- 默认info背景色和TabLine一致
+  -- 若TabLine有剩余空间，则背景色和TabLineFill一致
+  local info_color_group = 'TabLineInfo'
+  if total_char < capacity then
+    info_color_group = 'TabLineInfoFill'
+  end
+
+  s = s .. '%#TabLineFill#%T'
+  s = s .. '%=%#TabLine#'
+  s = string.format('%s%%#%s#%s', s, info_color_group, info)
+
+  return s
+end
+
 vim.o.tabline='%!v:lua.MyTabLine()'
