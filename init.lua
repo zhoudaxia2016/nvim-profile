e2c1c28f0fe106aae854844f8dbcac23ca867a3b
zhoudaxia <muzhouz@gmail.com>
2026-09-25 21:28:41 +0800

    feat: 切换目录时设置当前目录

--- init.lua
+++ init.lua
@@ -14,6 +14,18 @@ require 'config'
 require 'plugins'
 require 'features'
 
+local last
+vim.api.nvim_create_autocmd('DirChanged', {
+  callback = function(ev)
+    if ev.file == last then return end
+    last = ev.file
+    vim.schedule(function()
+      vim.fn.chansend(vim.v.stderr,
+        ('\027]7;file://%s%s\007'):format(vim.fn.hostname(), ev.file))
+    end)
+  end,
+})
+
 require('vim._core.ui2').enable({
   enable = true,
   msg = {
